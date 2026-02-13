01f5f9a33e457ff0072f5d78138e4e7bbd677063
Ben Suskins <suskinsdevelopment@gmail.com>
2026-02-13 19:52:40 +0000

    feat: add icon library and overhaul sidebar and navigation layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     "./templates/**/*_templ.go",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ['"Inter"', 'system-ui', '-apple-system', 'sans-serif'] },
+      boxShadow: { card: '0 1px 2px 0 rgb(0 0 0 / 0.03)' },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),

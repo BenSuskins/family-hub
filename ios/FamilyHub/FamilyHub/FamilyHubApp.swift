051303faa413e40d8325a4ee9535db1f21aa8593
Ben Suskins <suskinsdevelopment@gmail.com>
2026-03-13 09:32:18 +0000

    feat: add Xcode project skeleton with SwiftUI + familyhub:// URL scheme

---
+++ ios/FamilyHub/FamilyHub/FamilyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyHubApp.swift
+//  FamilyHub
+//
+//  Created by Ben Suskins on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FamilyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

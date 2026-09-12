d1057ea7ee0f49b951a9e2cf257801479e4830eb
Ben Suskins <suskinsdevelopment@gmail.com>
2026-09-12 17:45:43 +0100

    add scheme

---
+++ ios/FamilyHub/FamilyHubWatch Watch App/FamilyHubWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyHubWatchApp.swift
+//  FamilyHubWatch Watch App
+//
+//  Created by Ben Suskins on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct FamilyHubWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
